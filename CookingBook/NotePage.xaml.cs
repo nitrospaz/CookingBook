d8d72fa5cd9abc608f9c17323423eb1f50a13e80
nitrospaz <36904927+nitrospaz@users.noreply.github.com>
2024-09-22 10:49:30 -0600

    Create, save and delete a local note txt file

---
+++ CookingBook/NotePage.xaml.cs
@@ -0,0 +1,30 @@
+namespace CookingBook;
+
+public partial class NotePage : ContentPage
+{
+    // this is where the note will be stored. In a local text file.
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public NotePage()
+	{
+		InitializeComponent();
+
+        if (File.Exists(_fileName))
+            TextEditor.Text = File.ReadAllText(_fileName);
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        // Save the file.
+        File.WriteAllText(_fileName, TextEditor.Text);
+    }
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        // Delete the file.
+        if (File.Exists(_fileName))
+            File.Delete(_fileName);
+
+        TextEditor.Text = string.Empty;
+    }
+}
