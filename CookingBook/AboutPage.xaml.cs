f45992e2acc7cc86990deeaea37eb74e0bb0630f
nitrospaz <36904927+nitrospaz@users.noreply.github.com>
2024-09-22 10:21:00 -0600

    add about page

--- CookingBook/AboutPage.xaml.cs
+++ CookingBook/AboutPage.xaml.cs
@@ -6,4 +6,10 @@ public partial class AboutPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
 }
