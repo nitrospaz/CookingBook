7c96e1bd655c2ee2cb25e74aec9fd365610d2df5
nitrospaz <36904927+nitrospaz@users.noreply.github.com>
2024-09-22 17:02:34 -0600

    add all notes page and refactor to give each note a random name

--- CookingBook/AppShell.xaml.cs
+++ CookingBook/AppShell.xaml.cs
@@ -5,6 +5,13 @@
         public AppShell()
         {
             InitializeComponent();
+
+            // Register routes for the pages
+            // Every page that can be navigated to from another page,
+            // needs to be registered with the navigation system.
+            // The AllNotesPage and AboutPage pages are automatically registered
+            // with the navigation system by being declared in the TabBar.
+            Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
         }
     }
 }
--- CookingBook/Views/NotePage.xaml.cs
+++ CookingBook/Views/NotePage.xaml.cs
@@ -1,30 +1,53 @@
 namespace CookingBook.Views;
 
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage
 {
-    // this is where the note will be stored. In a local text file.
-    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+    public string ItemId
+    {
+        set { LoadNote(value); }
+    }
 
     public NotePage()
 	{
 		InitializeComponent();
 
-        if (File.Exists(_fileName))
-            TextEditor.Text = File.ReadAllText(_fileName);
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
+    }
+    private void LoadNote(string fileName)
+    {
+        Models.Note noteModel = new Models.Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
     }
 
-    private void SaveButton_Clicked(object sender, EventArgs e)
+    private async void SaveButton_Clicked(object sender, EventArgs e)
     {
-        // Save the file.
-        File.WriteAllText(_fileName, TextEditor.Text);
+        if (BindingContext is Models.Note note)
+            File.WriteAllText(note.Filename, TextEditor.Text);
+
+        await Shell.Current.GoToAsync("..");
     }
 
-    private void DeleteButton_Clicked(object sender, EventArgs e)
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
     {
-        // Delete the file.
-        if (File.Exists(_fileName))
-            File.Delete(_fileName);
+        if (BindingContext is Models.Note note)
+        {
+            // Delete the file.
+            if (File.Exists(note.Filename))
+                File.Delete(note.Filename);
+        }
 
-        TextEditor.Text = string.Empty;
+        await Shell.Current.GoToAsync("..");
     }
 }
