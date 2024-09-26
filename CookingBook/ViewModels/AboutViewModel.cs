e748d074a21f052442ec171c40cd2a35790ab792
nitrospaz <36904927+nitrospaz@users.noreply.github.com>
2024-09-26 14:19:19 -0600

    refactored to MVVM pattern

---
+++ CookingBook/ViewModels/AboutViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CookingBook.ViewModels
+{
+    internal class AboutViewModel
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+        // Command is equivalent to onClicked in Xamarin.Forms
+        public ICommand ShowMoreInfoCommand { get; }
+
+        public AboutViewModel()
+        {
+            ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+        }
+
+        async Task ShowMoreInfo() =>
+            await Launcher.Default.OpenAsync(MoreInfoUrl);
+    }
+}
