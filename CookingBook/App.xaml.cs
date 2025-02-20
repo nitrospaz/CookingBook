cd574cd1479aec46e0170ce3146a65fd25a98f43
nitrospaz <36904927+nitrospaz@users.noreply.github.com>
2025-02-20 13:07:28 -0700

    added if statements for compiling platform specific functions for now

--- CookingBook/App.xaml.cs
+++ CookingBook/App.xaml.cs
@@ -1,5 +1,12 @@
 ﻿using CookingBook.Services;
+// if it is not implemented for all platforms,
+// we need to specify the if for platform-specific implementation
+// otherwise it wont compile for other platforms
+#if ANDROID
+// this is only implemented for android right now
+// will probably need to be implemented for other platforms later...
 using FileService = CookingBook.Platforms.Android.Services.FileService;
+#endif
 
 namespace CookingBook
 {
@@ -8,10 +15,10 @@ namespace CookingBook
         public App()
         {
             InitializeComponent();
-
+#if ANDROID
             // Register the FileService with the DependencyService
             DependencyService.Register<IFileService, FileService>();
-
+#endif
             MainPage = new AppShell();
         }
     }
